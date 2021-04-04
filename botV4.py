31354673b814597d064503712fb606d74cb5c7c3
SuMaiKaDe <hbtsss@163.com>
2021-04-04 18:04:13 +0800

    修复 中文编码问题

--- botV4.py
+++ botV4.py
@@ -220,12 +220,8 @@ async def getname(path,dir):
         if os.path.isdir(path+'/'+file):
             names.append(file)
         else:
-            try:
-                with open(path+'/'+file,'r',encoding='utf-8') as f:
-                    resdatas = f.readlines()
-            except:
-                with open(path+'/'+file,'r',encoding='gbk') as f:
-                    resdatas = f.readlines()
+            with open(path+'/'+file,'r',encoding='utf_8_sig') as f:
+                resdatas = f.readlines()
             for data in resdatas:
                 if 'new Env' in data:
                     res = re.findall(reg,data)
