b28b6c966b5b8fc8230caf9d93a3edbb6d8cd56f
SuMaiKaDe <hbtsss@163.com>
2021-05-07 16:42:34 +0800

    版本号

--- jbot/ecosystem.config.js
+++ jbot/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "jbot",
-        version: "0.3.0",
+        version: "0.3.1",
         cwd: "..",
         script: "python",
         args: "-m jbot",
@@ -11,7 +11,10 @@ module.exports = {
             "jbot/__pycache__/*",
             "jbot/bot/__pycache__/*",
             "jbot/diy/__pycache__/*",
-            "jbot/*.log"
+            "jbot/*.log",
+            "jbot/*/*.log",
+            "jbot/requirements.txt",
+            "jbot/ecosystem.config.js"
         ],
         watch_delay: 15000,
         interpreter: ""
