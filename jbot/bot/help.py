f13e19fad0c0c740805ab9b96363b14f21ed6dec
HydraHail <8326343+Hydrahail-Johnson@users.noreply.github.com>
2021-05-27 02:52:05 +0800

    Update help.py
    增加命令说明

--- jbot/bot/help.py
+++ jbot/bot/help.py
@@ -12,15 +12,16 @@ async def myhelp(event):
     mhelp = '''
 a-我的自定义快捷按钮
 bean-获取收支
-edit-编辑文件
-start-开始使用本程序
-node-执行js脚本文件，绝对路径。
+chart-统计收支变化
 cmd-执行cmd命令
-snode-选择脚本后台运行
-log-选择日志
+edit-编辑文件
+getcookie-扫码获取cookie
 getfile-获取jd目录下文件
+log-选择日志
+node-执行js脚本文件，绝对路径
 setshort-设置自定义按钮
-getcookie-扫码获取cookie'''
+snode-选择脚本后台运行
+start-开始使用本程序'''
     bean = '/bean 加数字，获取该账户近期收支情况\n/bean in\out获取所有账户近期收或支情况\n/bean 获取账户总豆数量'
     cmd = '/cmd用于执行cmd命令，如果命令持续10分钟仍未结束，将强行终止，以保障机器人响应'
     edit = '/edit 进入/jd目录选择文件进行编辑，仅限简易编辑\n/edit /jd/config进入config目录选择文件编辑\n/edit /jd/config/config.sh 直接编辑config.sh文件'
@@ -29,7 +30,7 @@ getcookie-扫码获取cookie'''
     getfile = '/getfile 进入/jd目录选择文件进行获取\n/getfile /jd/config进入config目录选择文件获取\n/getfile /jd/config/config.sh 直接获取config.sh文件'
     setshort = '/setshort 用于设置快捷方式，格式如下：\n更新-->jup\nAAA-->BBB这种格式使用/a选择\n/bean 1\n/edit /jd/config/config.sh\n以“/”开头的为机器人命令快捷，使用/b选择'
     snode = '/snode 选择脚本并运行'
-    chart = ''
+    chart = '/chart 加数字，统计该账户近期收支情况'
     helpme = {'bean': bean, 'cmd': cmd, 'edit': edit, 'getcookie': getcookie, 'node': node,
               'getfile': getfile, 'setshort': setshort, 'snode': snode, 'chart': chart,'mhelp':mhelp}
     await jdbot.send_message(chat_id, helpme[text])
